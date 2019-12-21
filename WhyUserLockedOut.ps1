258300c2bdcea9d346d128609dbe4ebdc6fbc287
David M Williams <david@davidmwilliams.com>
2019-12-21 22:54:30 +1100

    Update WhyUserLockedOut.ps1
    Now supports multiple domain controllers

--- WhyUserLockedOut.ps1
+++ WhyUserLockedOut.ps1
@@ -1,9 +1,14 @@
-$properties = @(
+$DCs = Get-ADDomainController -filter * | Select-Object name
+$Properties = @(
     @{n='User';e={$_.Properties[0].Value}},
     @{n='Locked by';e={$_.Properties[1].Value}},
     @{n='TimeStamp';e={$_.TimeCreated}}
     @{n='DCname';e={$_.Properties[4].Value}}
 )
-Get-WinEvent -ComputerName DOMAINCONTROLLER1 -FilterHashTable @{LogName='Security'; ID=4740} | 
-Select $properties |
+
+Invoke-Command -ComputerName $DCs -ScriptBlock {
+    Get-WinEvent -FilterHashTable @{LogName='Security'; ID=4740} | 
+    Select $Using:Properties
+} |
+
 Export-csv C:\Users\$env:username\Desktop\LockedUsers.csv -NoTypeInformation -Encoding UTF8
